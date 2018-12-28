8c4b2bafaec41a95a2be48248c406835ef2a8989
x <x@zol.su>
2018-12-29 02:32:02 +0300

    add components

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="app">
+        <h2>{{message}}</h2>
+        <app-navigation></app-navigation>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'App',
+        data () {
+            return {
+                message: 'Welcome to Vue.js'
+            }
+        }
+    }
+</script>
+
+<style>
+    #hello {
+        font-family: Verdana;
+        color: navy;
+    }
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+import App from './App.vue'
+
+import 'materialize-css/dist/css/materialize.css';
+import 'materialize-css/dist/js/materialize.js';
+
+import AppNavigation from "./AppNavigation.vue"
+Vue.component('app-navigation', AppNavigation);
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
+})
